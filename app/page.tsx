ec56b7fe8960f3aa875be24f30adaf01f42783d3
fari4r <fari4r@gmail.com>
2024-09-11 22:58:35 +0330

    SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+// Example using <Head> in Next.js
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div>
-      Fariborz Rafatrad website fariborz rafatrad fariborz next js fariborz
-      rafat rad
-    </div>
+    <>
+      <Head>
+        <title>Fariborz rafatrad - Personal Website</title>
+        <meta
+          name="description"
+          content="A brief description about yourself."
+        />
+        <meta
+          name="keywords"
+          content="Fariborz rafatrad, personal website, portfolio, etc."
+        />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://fari4r.github.io/mynext/" />
+      </Head>
+      {/* Your website content */}
+    </>
   );
 }
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://fari4r.github.io/mynext/',
+  generateRobotsTxt: true,
+};
