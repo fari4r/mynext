5257c277a6935e48c14a3ba502bbbda8c857d701
fari4r <fari4r@gmail.com>
2024-09-13 01:38:39 +0330

    move meta to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,8 @@
-// Example using <Head> in Next.js
-import Head from "next/head";
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Fariborz rafatrad - Personal Website</title>
-        <meta
-          name="google-site-verification"
-          content="T40OzoESge6yMeJZanfCQfhXdmbVSuZt63VdiQFwbvY"
-        />
-        <meta
-          name="description"
-          content="Fariborz rafatrad, front-end developer"
-        />
-        <meta
-          name="keywords"
-          content="Fariborz rafatrad, personal website, portfolio,فریبرز , رفعت راد ,برنامه نویس فرانت اند , رفعت , راد"
-        />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://fari4r.github.io/mynext/" />
-      </Head>
-      <div>
-        <h1>I am Fariborz Rafatrad. I am a front-end developer.</h1>
-        <p>سلام من فریبرز رفعت راد هستم برنامه نویس فرانت اند.</p>
-      </div>
-    </>
+    <div>
+      <h1>I am Fariborz Rafatrad. I am a front-end developer.</h1>
+      <p>سلام من فریبرز رفعت راد هستم برنامه نویس فرانت اند.</p>
+    </div>
   );
 }
